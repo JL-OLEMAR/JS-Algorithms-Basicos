17832972102fd0598d0549fd001011aaddd1a988
JL-OLEMAR <joseluis19963@gmail.com>
2022-06-02 00:56:59 -0500

    Add one more exercise

--- exercise19/index.js
+++ exercise19/index.js
@@ -1,3 +1,20 @@
+// First Solution (Iterative)
+function validParentheses(parens) {
+  let count = 0;
+  for (let i = 0; i < parens.length; i++) {
+    if (parens[i] === '(') count++;
+    if (parens[i] === ')') count--;
+    if (count < 0) return false;
+  }
+  return count === 0;
+}
+
+validParentheses('()'); // => true
+validParentheses(')(()))'); // => false
+validParentheses('('); // => false
+validParentheses('(())((()())())'); // => true
+
+// Second Solution (Recursive)
 function validParentheses(parens) {
   if (parens === '') return true;
   const replaced = parens.replace(/\(\)/ig, '');
